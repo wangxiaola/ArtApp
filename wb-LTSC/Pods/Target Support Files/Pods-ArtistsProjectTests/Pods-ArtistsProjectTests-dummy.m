73ad5e852edf7ea2dffe080c11e5320ed5d2edbe
wangxiaola <1015559188@qq.com>
2018-02-23 10:05:47 +0800

    第一部分

---
+++ wb-LTSC/Pods/Target Support Files/Pods-ArtistsProjectTests/Pods-ArtistsProjectTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ArtistsProjectTests : NSObject
+@end
+@implementation PodsDummy_Pods_ArtistsProjectTests
+@end
---
+++ wb-LTSC/Pods/Target Support Files/Pods-ArtistsProjectUITests/Pods-ArtistsProjectUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ArtistsProjectUITests : NSObject
+@end
+@implementation PodsDummy_Pods_ArtistsProjectUITests
+@end
